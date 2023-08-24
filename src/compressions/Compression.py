GPU big fix sparse gradients

src/compressions/Compression.py
@@ -1,6 +1,11 @@
 import abc
+
+import numpy as np
 import tensorflow as tf
 from tensorflow import Tensor
+from typing import Final
+
+
 class Compression:
     def __init__(self, name: str):
         self.name = name
@@ -26,7 +31,7 @@ class Compression:
         self._build_index_dict(var_list)
 
     def add_variable_from_reference(
-        self, model_variable, variable_name, shape=None, initial_value=None
+            self, model_variable, variable_name, shape=None, initial_value=None
     ):
         """Create an optimizer variable from model variable.
 
@@ -89,10 +94,41 @@ class Compression:
         """
         # k = tf.cast(k, tf.int32.base_dtype)
 
-        input_shape = input_tensor.shape
+        input_shape: Final = input_tensor.shape
         flattened_tensor: Tensor = tf.reshape(input_tensor, [-1])
-        if tf.size(flattened_tensor) < k:
+
+        if tf.size(flattened_tensor) <= k:
             return input_tensor
+
+        ft_np = tf.abs(flattened_tensor).numpy()
+
+        indices = np.argpartition(np.abs(ft_np.ravel()), -k)[-k:]
+        mask = tf.zeros(flattened_tensor.shape, dtype=flattened_tensor.dtype)
+        mask = tf.tensor_scatter_nd_update(mask, tf.expand_dims(indices, axis=1),
+                                           tf.ones_like(indices, dtype=tf.float32))
+
+        spars_tensor = flattened_tensor * mask
+
+        spars_tensor = tf.reshape(spars_tensor, input_shape)
+
+        return spars_tensor
+
+        # sorted_tensor = -tf.sort(-tf.abs(flattened_tensor))
+        #
+        # # binary search
+        # low, high = 0, k
+        # while low < high:
+        #     mid = (low + high) // 2
+        #     if sorted_tensor[mid] >= sorted_tensor[k]:
+        #         low = mid + 1
+        #     else:
+        #         high = mid
+        # threshold = sorted_tensor[high - 1]
+        #
+        # mask = tf.math.greater(tf.abs(flattened_tensor), threshold)
+        # gradient_dropped = flattened_tensor * tf.cast(mask, flattened_tensor.dtype)
+        # gradient_dropped = tf.reshape(gradient_dropped, input_shape)
+        # return gradient_dropped
 
         abs_tensor = tf.abs(flattened_tensor)
         # s = tf.sort(tf.reshape(abs_tensor, [-1]), direction="DESCENDING")

src/compressions/SparseGradient.py
@@ -4,6 +4,8 @@ import tensorflow_probability as tfp
 from tensorflow import Tensor
 
 from .Compression import Compression
+
+
 # from ..utilities.huffman import *
 
 
@@ -30,7 +32,7 @@ class SparseGradient(Compression):
                 model_variable=var, variable_name="residual"
             )
 
-        #self.compression_rate = var_list[0].dtype.size * 8
+        # self.compression_rate = var_list[0].dtype.size * 8
         self._built = True
 
     def compress(self, gradient: Tensor, variable) -> Tensor:
@@ -46,17 +48,23 @@ class SparseGradient(Compression):
         self.residuals[variable.ref()].assign(gradient - gradient_dropped)
 
         if variable.ref() not in self.cr:
-            self.cr[variable.ref()] = gradient.dtype.size * 8 * np.prod(gradient.shape.as_list()) / self.get_sparse_tensor_size_in_bits(
+            self.cr[variable.ref()] = gradient.dtype.size * 8 * np.prod(
+                gradient.shape.as_list()) / self.get_sparse_tensor_size_in_bits(
                 gradient_dropped)
             self.compression_rates.append(self.cr[variable.ref()])
 
         return gradient_dropped
 
-    @staticmethod
-    def gradDrop(gradient: Tensor, drop_rate) -> Tensor:
+    def gradDrop(self, gradient: Tensor, drop_rate) -> Tensor:
         """
         Updates by removing drop_rate % of the smallest gradients by absolute value
         """
-        threshold = tfp.stats.percentile(tf.abs(gradient), q=drop_rate, interpolation="lower")
-        gradient_dropped = tf.where(tf.abs(gradient) > threshold, gradient, 0)
+        # threshold = tfp.stats.percentile(tf.abs(gradient), q=drop_rate, interpolation="lower")
+        # gradient_dropped = tf.where(tf.abs(gradient) > threshold, gradient, 0)
+        # return gradient_dropped
+
+        flattened_tensor: Tensor = tf.reshape(gradient, [-1])
+        k = int(np.ceil(flattened_tensor.shape[0] * (1 - drop_rate / 100)))
+
+        gradient_dropped = self.top_k_sparsification(gradient, k)
         return gradient_dropped

src/compressions/TopK.py
@@ -27,7 +27,8 @@ class TopK(Compression):
         sparse_gradient = self.top_k_sparsification(gradient, self.k)
 
         if variable.ref() not in self.cr:
-            self.cr[variable.ref()] = gradient.dtype.size * 8 * np.prod(gradient.shape.as_list()) / self.get_sparse_tensor_size_in_bits(
+            self.cr[variable.ref()] = gradient.dtype.size * 8 * np.prod(
+                gradient.shape.as_list()) / self.get_sparse_tensor_size_in_bits(
                 sparse_gradient)
             self.compression_rates.append(self.cr[variable.ref()])
 

src/compressions/tests/test_SparseGradient.py
@@ -1,6 +1,5 @@
 import unittest
 from src.compressions.SparseGradient import *
-from src.utilities.compression_rate import get_sparse_tensor_size_in_bits
 
 
 class TestSparseGradient(unittest.TestCase):
@@ -10,10 +9,9 @@ class TestSparseGradient(unittest.TestCase):
         calc = sg.gradDrop(grad, 99)
         calc = tf.cast(calc, dtype=tf.float32)
         print(grad, calc)
-        print(get_sparse_tensor_size_in_bits(grad)/get_sparse_tensor_size_in_bits(calc))
 
     def test_gradDrop2d(self):
-        sg = SparseGradient(drop_rate=50)
+        sg = SparseGradient(drop_rate=10)
         calc = sg.gradDrop(tf.constant([[0.3, -1.2, 0.9, 0.2], [0.3, 2, 0.9, 0.2]],
                                        dtype=tf.float32), 50)
         print(calc)

src/optimizer/MemSGD.py
@@ -70,20 +70,23 @@ class MemSGD(Optimizer):
         Returns a sparse tensor of the input tensor, with the top k elements.
         k = 2: [1, 2, 3] -> [0, 2, 3]
         """
+        # k = tf.cast(k, tf.int32.base_dtype)
+
         input_shape = input_tensor.shape
         flattened_tensor: Tensor = tf.reshape(input_tensor, [-1])
 
-        if tf.size(flattened_tensor) < k:
+        if tf.size(flattened_tensor) <= k:
             return input_tensor
 
-        abs_tensor = tf.abs(flattened_tensor)
-        indices = tf.math.top_k(abs_tensor, k).indices
+        ft_np = tf.abs(flattened_tensor).numpy()
 
-        mask = tf.zeros(flattened_tensor.shape)
+        indices = np.argpartition(np.abs(ft_np.ravel()), -k)[-k:]
+        mask = tf.zeros(flattened_tensor.shape, dtype=flattened_tensor.dtype)
         mask = tf.tensor_scatter_nd_update(mask, tf.expand_dims(indices, axis=1),
                                            tf.ones_like(indices, dtype=tf.float32))
 
-        spars_tensor = tf.math.multiply(flattened_tensor, mask)
+        spars_tensor = flattened_tensor * mask
+
         spars_tensor = tf.reshape(spars_tensor, input_shape)
 
         return spars_tensor