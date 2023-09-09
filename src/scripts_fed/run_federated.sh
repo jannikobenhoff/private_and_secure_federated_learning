9c6468ce15aba0121624beb92f6241f5abfc0c42
Jannik Obenhoff <hello@jannikobenhoff.com>
2023-09-09 09:42:10 +0200

    update

--- src/scripts_fed/run_federated.sh
+++ src/scripts_fed/run_federated.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-base_strategy='{"optimizer": "None", "compression": "topk", "learning_rate": 0.1, "k": 1000}'
 #
 #base_strategy='{"optimizer": "None", "compression": "terngrad", "learning_rate": 0.1, "clip": 2.5}'
 #
@@ -8,10 +7,10 @@ base_strategy='{"optimizer": "sgd", "compression": "none", "learning_rate": 0.01
 ###
 #base_strategy='{"optimizer": "sgd", "compression": "onebitsgd", "learning_rate": 0.05}'
 #
-#base_strategy='{"optimizer": "sgd", "compression": "sparsegradient", "learning_rate": 0.01, "drop_rate": 99}'
+#sparsegradient='{"optimizer": "sgd", "compression": "sparsegradient", "learning_rate": 0.01, "drop_rate": 99}'
 #base_strategy='{"optimizer": "sgd", "compression": "bsgd", "learning_rate": 0.01, "buckets": 1000, "sparse_buckets": 999}'
-#base_strategy='{"optimizer": "sgd", "compression": "vqsgd", "learning_rate": 0.01, "repetition": 100}'
-#base_strategy='{"optimizer": "sgd", "compression": "gradientsparsification", "learning_rate": 0.01, "max_iter": 2, "k": 0.005}'
+vqsgd='{"optimizer": "sgd", "compression": "vqsgd", "learning_rate": 0.01, "repetition": 250}'
+gradientsparsification='{"optimizer": "sgd", "compression": "gradientsparsification", "max_iter": 2, "k": 0.01}'
 #
 #base_strategy='{"optimizer": "memsgd", "compression": "none", "learning_rate": 0.01, "top_k": 1000}'
 ##
@@ -24,20 +23,30 @@ fetchsgd='{"optimizer": "fetchsgd", "compression": "none", "learning_rate": 0.05
 sgd='{"optimizer": "sgd", "compression": "none"}'
 onebitsgd='{"optimizer": "sgd", "compression": "onebitsgd"}'
 terngrad='{"optimizer": "None", "compression": "terngrad", "clip": 2.5}'
+topk='{"optimizer": "None", "compression": "topk", "learning_rate": 0.1, "k": 500}'
 
-base_strategy=$efsignsgd
+base_strategy=$sgd
 
-beta_values=(2 0.125)
-local_iter_types=(same dirichlet)
+beta_values=(0.125 2)
+local_iter_types=(dirichlet same)
+
+# dirichlet 2    -> 500
+# dirichlet 0125 -> 650
+# same 2         -> 500
+# same 0125      -> 500
 
 for beta in "${beta_values[@]}"; do
   for local_iter_type in "${local_iter_types[@]}"; do
+    max_iter=400
+    if [[ "$beta" == "0.125" && "$local_iter_type" == "dirichlet" ]]; then
+      max_iter=650
+    fi
     python ../main_federated.py --model lenet --dataset mnist \
-      --max_iter=400 \
+      --max_iter=$max_iter \
       --gpu=0 \
       --fullset=100 \
       --batch_size=500 \
-      --learning_rate=0.03 \
+      --learning_rate=0.05 \
       --stop_patience=7 \
       --beta="$beta" \
       --split_type=dirichlet \
